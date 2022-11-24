03cf896e615a2213440e6fa0d6a7cd7a45de103c
kzshaon <lockh193@gmail.com>
2022-11-25 01:35:26 +0600

    i've started a new project and finishedn slider section

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+// HERO SLIDER 
+$('#hero-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots: false,
+    items: 1,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+           
+        },
+        600:{
+           
+        },
+        1000:{
+            
+        }
+    }
+})
+});

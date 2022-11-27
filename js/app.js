b6c484b6c72cb8d0b3fcba36a02ba435190ed31a
kzshaon <lockh193@gmail.com>
2022-11-28 01:02:27 +0600

    I've finished project section

--- js/app.js
+++ js/app.js
@@ -10,14 +10,34 @@ $(document).ready(function(){
         navText: ['PREV', 'NEXT'],
         responsive:{
             0:{
-               
+               nav: false,
             },
-            600:{
-               
-            },
-            1000:{
-                
+            768: {
+                nav: true,
             }
         }
     })
-    });
+
+    // PROJECT SLIDER //
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: true,
+        smartSpeed: 1000,
+        navText: ['PREV', 'NEXT'],
+        margin: 24,
+        responsive:{
+            0:{
+              items: 2,
+            },
+            768: {
+              items: 3, 
+            },
+            1140: {
+                items: 2,
+                center: true,
+            }
+        }
+    })
+});

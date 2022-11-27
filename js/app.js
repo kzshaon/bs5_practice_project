d5276d4e80dcfd90ce384182ba76d17cbc54fd7f
kzshaon <lockh193@gmail.com>
2022-11-27 22:48:58 +0600

    this is for test

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-// HERO SLIDER 
-$('#hero-slider').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    dots: false,
-    items: 1,
-    smartSpeed: 1000,
-    responsive:{
-        0:{
-           
-        },
-        600:{
-           
-        },
-        1000:{
-            
+    // HERO SLIDER 
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        items: 1,
+        smartSpeed: 1000,
+        navText: ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+               
+            },
+            600:{
+               
+            },
+            1000:{
+                
+            }
         }
-    }
-})
-});
+    })
+    });
